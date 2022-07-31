e675594ff5b6d867579e80c2d6bbed0031c29525
salmayaser <salmayasser161@gmail.com>
2022-07-31 10:50:43 +0200

    Adding transactions

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import './Models/transaction.model.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(MyApp());
@@ -37,7 +38,34 @@ class MyHomePage extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
                 ...transactions.map((trans) {
-                  return Text(trans.title);
+                  return (Card(
+                    child: Row(children: [
+                      Container(
+                        margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                        child:
+                            Text('\$ ${trans.amount}', style: TextStyle(color: Colors.purple, fontWeight: FontWeight.bold, fontSize: 20)),
+                        decoration: BoxDecoration(border: Border.all(width: 2, color: Colors.purple)),
+                      ),
+                      Container(
+                        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 0),
+                        margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                        child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                trans.title,
+                                style: TextStyle(color: Colors.purple, fontWeight: FontWeight.bold, fontSize: 18),
+                              ),
+                              Text(
+                                DateFormat().add_yMMMd().format(trans.date),
+                                style: TextStyle(color: Colors.grey),
+                              )
+                            ]),
+                      )
+                    ]),
+                  ));
                 }).toList()
               ],
             )
